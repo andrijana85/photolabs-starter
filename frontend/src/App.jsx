1fe048e7827c04ba2daf8db048c6cf6510839c5b
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-14 23:13:25 -0400

    Add TopNavigationBar and HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoList from 'components/PhotoList';
-import PhotoFavButton from 'components/PhotoFavButton';
-import TopicList from './components/TopicList';
+import HomeRoute from 'routes/HomeRoute';
 
 
 // Note: Rendering a single component to build components in isolation
@@ -11,8 +8,7 @@ const App = () => {
  
   return (
     <div className="App">
-      <TopicList />
-     <PhotoList />
+       <HomeRoute />
     </div>
   );
 };
