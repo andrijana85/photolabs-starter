6fb579fbe1b349d84b947effcf1a446359179826
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-20 14:55:19 -0400

    Add Modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import { useState } from 'react';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
- 
+ // create a state variable to control the modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  
   return (
     <div className="App">
        <HomeRoute />
+       {isModalOpen && <PhotoDetailsModal />}
     </div>
   );
 };
