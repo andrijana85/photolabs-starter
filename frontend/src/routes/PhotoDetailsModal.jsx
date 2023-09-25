b837dded62cf7f02617afa45db49e0b8a4ab8880
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-25 11:23:55 -0400

    Diplay similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,6 +4,7 @@ import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoListItem from "components/PhotoListItem";
 import PhotoList from "components/PhotoList";
+import PhotoFavButton from "components/PhotoFavButton";
 
 const PhotoDetailsModal = ({
   closeModal,
@@ -13,7 +14,10 @@ const PhotoDetailsModal = ({
   openModal,
   favorites,
   photos,
+  photoId,
 }) => {
+  const { urls, location, user, similar_photos} = selectedPhoto;
+  const similarPhotos = Object.values(similar_photos);
   return (
     <div className="photo-details-modal">
       <button
@@ -22,7 +26,7 @@ const PhotoDetailsModal = ({
       >
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <div className="photo-details-modal__image">
+      {/* <div className="photo-details-modal__images">
       <PhotoListItem
         toggleFavorite={toggleFavorite}
         isFavorite={isFavorite}
@@ -31,15 +35,42 @@ const PhotoDetailsModal = ({
         openModal={() => openModal(selectedPhoto)}
         similarPhotos={selectedPhoto.similar_photos}
       />
-      </div>
+      </div> */}
       <div className="photo-details-modal__images">
+      <PhotoFavButton
+        toggleFavorite={toggleFavorite}
+        isFavorite={isFavorite}
+        photoId={photoId}
+      />
+      <img
+        src={urls.regular}
+        alt={`photo taken in ${location.city}, ${location.country}`}
+        className="photo-details-modal__image"
+        onClick={openModal}
+      />
+
+<div className="photo-details-modal__header photo-details-modal__photographer-details">
+        <img
+          src={user.profile}
+          alt={`profile of ${user.name}`}
+          className="photo-list__user-profile"
+        />
+          <div className="photo-details-modal__photographer-details">
+          {user.name}
+          <div className="photo-list__user-location">
+            {location.city}, {location.country}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="photo-details-modal__images">
         <h3>Similar Photos</h3>
         
       <PhotoList
         favorites={favorites}
         toggleFavorite={toggleFavorite}
         isFavorite={isFavorite}
-        photos={photos}
+        photos={similarPhotos}
         openModal={openModal}
         className="photo-details-modal__images"
       />
